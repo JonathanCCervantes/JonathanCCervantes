caa5c1ef25e628573f1fb120745bc42b97018184
Jonathan Cervantes <jonathancolasito.cervantes@gmail.com>
2024-06-30 14:54:37 +0800

    Update scripts.js
    updated scripts

--- scripts.js
+++ scripts.js
@@ -1,67 +1,403 @@
-jQuery(document).ready(function($) {
-    // Function to handle highlighting the active card
-    function setActiveCard(index) {
-        const cards = document.querySelectorAll('.card-1');
-        cards.forEach((card, i) => {
-            card.classList.remove('active-card');
-            if (i === index) {
-                card.classList.add('active-card');
-            }
-        });
-    }
+body {
+    background-color: #2D2E32;   
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
-    // Initialize the first card as active
-    let activeCardIndex = 0;
-    setActiveCard(activeCardIndex);
+.header{
+    padding: 0;
+    width: 1121px;
+    height: 48px;
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
-    // Event listeners for the buttons
-    document.getElementById('btn-left').addEventListener('click', () => {
-        if (activeCardIndex > 0) {
-            activeCardIndex--;
-            setActiveCard(activeCardIndex);
-        }
-    });
+.logo {
+    margin: 20px;
+}
 
-    document.getElementById('btn-right').addEventListener('click', () => {
-        const cards = document.querySelectorAll('.card-1');
-        if (activeCardIndex < cards.length - 1) {
-            activeCardIndex++;
-            setActiveCard(activeCardIndex);
-        }
-    });
+.logo img{
+    height: 34px;   
+}
 
-    // Function to change tabs
-    function changeTab(tabNumber) {
-        $('.leaf-tab').removeClass('active');
-        $('.leaf-tab:nth-child(' + tabNumber + ')').addClass('active');
+.nav { 
+    display: flex;
+    align-items: center;
+    border-bottom: 3px solid #242527;
+}
+.heading {
+    margin-left: 38px;
+}
 
-        $('.tab-section').hide();
-        $('#tab' + tabNumber + '-content').show();
-    }
+.heading p{
+    font-size: 17px;
+    color: white;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    font-weight: 500;
+}
 
-    // Function to update images
-    function updateImages() {
-        $('.leaf-tab').each(function() {
-            var $tab = $(this);
-            var $img = $tab.find('img');
-            var activeImg = $tab.data('active-img');
-            var inactiveImg = $tab.data('inactive-img');
-            if ($tab.hasClass('active')) {
-                $img.attr('src', activeImg);
-            } else {
-                $img.attr('src', inactiveImg);
-            }
-        });
-    }
+.vert-line{
+    width: 3px;
+    height: 40px;
+    background-color:#242527;
+    border-radius: 100px;
+    margin-right: 38px;
+    margin-left: 38px;
+}
 
-    // Initialize the first tab as active
-    changeTab(1);
-    updateImages();
+.button{
+    display: flex;
+    align-items: center;
+    margin-right: 81px;
+}
 
-    // Event listener for tab clicks
-    $('.leaf-tab').click(function() {
-        var tabIndex = $(this).index() + 1;
-        changeTab(tabIndex);
-        updateImages();
-    });
-});
+.button-end{
+    margin-right: 20px;
+}
+
+.button-end img {
+    height: 22px;
+    width: auto;
+}
+
+.button img {
+    height: 22px;
+    width: auto;
+}
+
+.container {
+    padding: 0;
+    width: 1121px;
+    height: 700px;
+    margin: 100px;
+    margin-top: 15px;
+}
+
+.section-1{
+    margin: 0;
+    width: 1121px;
+    height: 232px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.profile{
+    position: relative;
+    width: 339px;
+    height: 232px;
+    background-color: #313237;
+    box-shadow: 5px 14px 14.7px 2px #0000005C;
+    border-radius: 11px;
+    background-image: url(assets/Group\ 42.png);
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-position-x: left;
+    overflow: visible;
+}
+
+.profile img{
+    width: 288px;
+    height: 282px;
+    position: absolute;
+    top: -50px;
+    left: 40%;
+    transform: translateX(-50%);
+}
+
+.famw{
+    width: 349px;
+    height: 232px;
+    background-color: #313237;
+    box-shadow: 5px 14px 14.7px 2px #0000005C;
+    border-radius: 11px;
+}
+
+.famw h3{
+    color: white;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    margin-left: 29px;
+    margin-bottom: 0px;
+    font-size: 21px;
+    font-weight: 700;
+    text-align: left;
+}
+
+.skill-cont{
+    margin-top: 7px;
+    height: 150px;
+    width: 280px;
+    margin-left: 32px;
+    border-left: 2px dotted white;
+    display: flex;
+    flex-direction: column;
+}
+
+.holder{
+    display: flex;
+    justify-content: space-between; 
+}
+
+.skills{
+    width: 215px;
+    margin-left: 18px;
+    height: 140px;
+}
+
+.skills p{
+    color: white;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 10px;
+}
+
+.dot-colors{
+    display: flex;
+    flex-direction: column;
+    width: 8px;
+    margin-right: 3px;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+}
+
+.dot-colors .one{
+    background-color: #1DFF4D;
+    height: 8px;
+    border-radius: 100px;
+    margin-top: 19px;
+}
+
+.dot-colors .two{
+    background-color: #52C901;
+    height: 8px;
+    border-radius: 100px;
+    margin-top: 18px;
+}
+
+.dot-colors .three{
+    background-color: #888A01;
+    height: 8px;
+    border-radius: 100px;
+    margin-top: 18px;
+}
+
+.dot-colors .four{
+    background-color: #BF4B01;
+    height: 8px;
+    border-radius: 100px;
+    margin-top: 18px;
+}
+
+.dot-colors .five{
+    background-color: #F20F01;
+    height: 8px;
+    border-radius: 100px;
+    margin-top: 18px;
+}
+
+.bar{
+    background-color: #1F2024;
+    height: 10px;
+    margin-left: 18px;
+    margin-right: 18px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 0px;
+}
+
+.bar1{
+    background-color: #1DFF4D;
+    height: 8px;
+    margin-left: 0px;
+    margin-right: 18px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 1px;
+}
+
+.bar2{
+    background-color: #52C901;
+    height: 8px;
+    width: 160px;
+    margin-left: 0;
+    margin-right: 20px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 1px;
+}
+
+.bar3{
+    background-color: #888A01;
+    height: 8px;
+    width: 140px;
+    margin-left: 0px;
+    margin-right: 20px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 1px;
+}
+
+.bar4{
+    background-color: #BF4B01;
+    height: 8px;
+    width: 86px;
+    margin-left: 0px;
+    margin-right: 20px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 1px;
+}
+
+.bar5{
+    background-color: #F20F01;
+    height: 8px;
+    width: 60px;
+    margin-left: 0px;
+    margin-right: 20px;
+    border-radius: 50px;
+    padding: 0px;
+    margin-top: 1px;
+}
+
+.recw {
+    width: 385px;
+    height: 232px;
+    background-color: #313237;
+    box-shadow: 5px 14px 14.7px 2px #0000005C;
+    border-radius: 11px;
+}
+
+.recw h3 {
+    color: white;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    margin-left: 29px;
+    font-size: 21px;
+    font-weight: 700;
+    line-height: 25.41px;
+    text-align: left;
+}
+
+.proj {
+    width: 319px;
+    height: 83px;
+    margin-left: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.proj-one {
+    height: 19px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.proj-one p {
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    color: white;
+    font-size: 16px;
+}
+
+.proj-two {
+    height: 19px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.proj-two p {
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    color: white;
+    font-size: 16px;
+}
+
+.proj-three {
+    height: 19px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.proj-three p {
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    color: white;
+    font-size: 16px;
+}
+
+.proj-title {
+    width: 201px;
+    height: 19px;
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+    box-sizing: border-box;
+}
+
+.proj-btn {
+    background-color: rgb(19, 17, 100);
+    width: 54px;
+    height: 19px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+}
+
+.email-area{
+    margin-top: 5.5px;
+    height: 51px;
+    width: 318px;
+    margin-left: 32px;
+}
+
+.qst-email{
+    height: 12px;
+    width: 200px;
+    margin: 0px;
+    padding: 0px;
+}
+
+.qst-email p{
+    margin: 0px;
+    padding: 0px;
+    font-size: 10px;
+    color: white;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+}
+
+.email-me{
+    height: 31px;
+    width: 318px;
+    padding: 0;
+    padding-top: 8px;
+    border-radius: 6px;
+    align-self: center;
+    background: linear-gradient(90deg, #FF7A00 -38.55%, #FF4117 42.23%, #FF0099 126.34%);
+    cursor: pointer;
+    text-align: center;
+}
+
+.email-area a{
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+    text-decoration: none;
+    color: white;
+    margin: 0%;
+    padding: 0%;
+    font-size: 10px;
+}
