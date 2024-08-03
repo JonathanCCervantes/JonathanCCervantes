9b52e2319467237ecf2500df39e44a4f5911d409
Jonathan Cervantes <jonathancolasito.cervantes@gmail.com>
2024-08-03 21:26:17 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,7 +16,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     setupModal('openModalBtn', '#closeModalBtn', '#modal');
     setupModal('openModalBtntwo', '.close-modal2 a', '.modal-body2');
     setupModal('openModalBtnThree', '.close-modal3 a', '.modal-body3');
+    setupModal('openModalBtnFour', '.close-modal4 a', '.modal-body4');
+    setupModal('openModalBtnFive', '.close-modal5 a', '.modal-body5');
     setupModal('openModalBtnp1', '.close-modalp1 a', '.modal-bodyp1');
     setupModal('openModalBtnp3', '.close-modalp3 a', '.modal-bodyp3'); // Add setup for modal-bodyp3
     setupModal('openModalBtnp4', '.close-modalp4 a', '.modal-bodyp4');
+
+    document.getElementById('emailButton').addEventListener('click', function() {
+        const email = 'jonathancolasito.cervantes@gmail.com';
+        const subject = 'Subject here';
+        const body = 'Body content here';
+        const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+        window.open(gmailUrl, '_blank');
+    });
 });
